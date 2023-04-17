d14303f476c11103a5a442e23e60503a5cd8d3b9
wangwenjie <hifof@126.com>
2023-04-16 21:27:46 -0400

    Initial Commit

---
+++ EatItAll/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EatItAll
+//
+//  Created by 王文杰 on 4/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EatItAll/EatItAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatItAllApp.swift
+//  EatItAll
+//
+//  Created by 王文杰 on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EatItAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
