7319d961017e12ae9685985cf604d1966f51f8a1
wangwenjie <hifof@126.com>
2023-04-22 18:55:50 -0700

    Add project base construction

---
+++ EatItAll/FavoritePage.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritePage.swift
+//  EatItAll
+//
+//  Created by 王文杰 on 4/22/23.
+//
+
+import SwiftUI
+
+struct FavoritePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritePage_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritePage()
+    }
+}
---
+++ EatItAll/SeeAllPage.swift
@@ -0,0 +1,20 @@
+//
+//  SeeAllPage.swift
+//  EatItAll
+//
+//  Created by 王文杰 on 4/22/23.
+//
+
+import SwiftUI
+
+struct SeeAllPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SeeAllPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SeeAllPage()
+    }
+}
